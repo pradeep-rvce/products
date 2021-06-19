0ac0d0c9767d798dcfcb16fd201262b18d24f03f
pradeepm <pradeep.mani@joinmosaic.com>
2021-06-19 13:01:30 +0530

    edge

--- kubernetes/scripts/deploy-dev-env.bash
+++ kubernetes/scripts/deploy-dev-env.bash
@@ -2,6 +2,12 @@
 
 # Print commands to the terminal before execution and stop the script if any error occurs
 set -ex
+Kubectl delete namespace hands-on
+Kubectl create namespace hands-on
+Kubectl delete namespace logging
+Kubectl create namespace logging
+
+kubectl config set-context --current --namespace=hands-on
 
 kubectl create configmap config-repo-auth-server       --from-file=config-repo/application.yml --from-file=config-repo/auth-server.yml --save-config
 kubectl create configmap config-repo-gateway           --from-file=config-repo/application.yml --from-file=config-repo/gateway.yml --save-config
@@ -9,6 +15,8 @@ kubectl create configmap config-repo-product-composite --from-file=config-repo/a
 kubectl create configmap config-repo-product           --from-file=config-repo/application.yml --from-file=config-repo/product.yml --save-config
 kubectl create configmap config-repo-recommendation    --from-file=config-repo/application.yml --from-file=config-repo/recommendation.yml --save-config
 kubectl create configmap config-repo-review            --from-file=config-repo/application.yml --from-file=config-repo/review.yml --save-config
+kubectl create configmap prometheus                    --from-file=config-repo/prometheus.yml --save-config -n logging
+kubectl create configmap datasource                    --from-file=config-repo/datasource.yml --save-config -n logging
 
 
 kubectl create secret generic mongodb-server-credentials \
@@ -46,5 +54,14 @@ kubectl wait --timeout=600s --for=condition=ready pod --all
 # Next deploy the microservices and wait for their pods to become ready
 kubectl apply -k kubernetes/services/overlays/dev
 kubectl wait --timeout=600s --for=condition=ready pod --all
+
+
+kubectl apply -f kubernetes/efk/fluentd-hands-on-configmap.yml -n kube-system
+kubectl apply -f kubernetes/efk/fluentd-ds.yml -n kube-system
+
+kubectl apply -f kubernetes/efk/elasticsearch.yml -n logging
+kubectl apply -f kubernetes/efk/kibana.yml -n logging
+kubectl apply -f kubernetes/pg/prometheus.yml -n logging
+kubectl apply -f kubernetes/pg/grafana.yml -n logging
 
 set +ex
